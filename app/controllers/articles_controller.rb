e784756ae19ae82c5236dfa0e1d21ed41a89a215
Thom <thomybanez@yahoo.com>
2023-03-14 13:50:53 +0800

    Working Rails

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,45 +1,44 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
 
-    def index
-        @articles = Article.all
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+  
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def new
-        @article = Article.new
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+  
+    if @article.update(article_params)
+      redirect_to articles_path
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
+  
+  def delete
+    @article = Article.find(params[:id])
+    @article.destroy   
+    redirect_to articles_path
+  end
 
-    def create
-        @article = Article.new(article_params)
-      
-        if @article.save
-          redirect_to articles_path
-        else
-          render :new, status: :unprocessable_entity
-        end
-      end  
-
-    def edit
-        @article = Article.find(params[:id])
-    end
-
-    def update
-        @article = Article.find(params[:id])
-      
-        if @article.update(article_params)
-          redirect_to articles_path
-        else
-          render :edit, status: :unprocessable_entity
-        end
-    end
-
-    def delete
-        @article = Article.find(params[:id])
-        @article.destroy   
-        redirect_to articles_path
-    end
-
-    private
-    def article_params
-        params.require(:article).permit(:name,:body)
-    end
+  private
+  def article_params
+    params.require(:article).permit(:name, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
 
   get '/articles' => 'articles#index'
   get '/articles/new' => 'articles#new', as: 'new_article'
   post '/articles' => 'articles#create', as: 'create_article'
 
   get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
-  patch '/articles/:id' => 'articles#update'
-  delete 'articles/:id' => 'articles#destroy'
+  patch '/articles/:id' => 'articles#update', as: 'update_article'
+  delete '/articles/:id' => 'articles#delete', as: 'delete_article'
 
 end
